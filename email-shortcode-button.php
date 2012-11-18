f17a5814cb193f911628cb855d66c76fb8a2eacc
Tibor Paulsch <4tibor@gmail.com>
2012-11-18 09:25:42 +0100

    First commit

---
+++ email-shortcode-button.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Plugin Name: Anti SpamBot Email Links
+ * Description: Adds a shortcode button to the TinyMCE editor for easy anti spambot email links
+ * Version: 1.0
+ * Author: Tibor Paulsch
+ * Author URI: http://i-tibor.nl/
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+// Add shortcode [email] for email antispam links
+add_shortcode( 'email', 'tibs_sc8_email' );
+function tibs_sc8_email( $attr, $content ) {
+	if( is_email( $content ) ) {
+		$content = antispambot( $content );
+		return sprintf( '<a href="mailto:%s">%s</a>', $content, $content );
+	}
+	else {
+		return '';
+	}
+}
+
+// Add a button to the post editor for the email shortcode
+add_action( 'init', 'tibs_add_button' );
+function tibs_add_button() {
+	if ( current_user_can('edit_posts') &&  current_user_can('edit_pages') )
+	{
+		add_filter('mce_external_plugins', 'tibs_add_plugin');
+		add_filter('mce_buttons', 'tibs_register_button');
+	}
+}
+
+function tibs_add_plugin($plugin_array) {
+	$plugin_array['emaillink'] = plugin_dir_url(__FILE__).'lib/customcodes.js';
+	
+	return $plugin_array;
+}
+
+function tibs_register_button($buttons) {
+	array_push($buttons, "emaillink");
+	
+	return $buttons;
+}
+
+
+
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Anti SpamBot Email Button ===
+Tags: email, spam, shortcode
+Tested up to: 3.4.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a shortcode button to the TinyMCE editor for easy anti spambot email links.
+
+== Description ==
+
+This simple plugin adds a button to the TinyMCE editor that puts a selected email adress into a shortcode [email], which uses WordPress' native antispambot()function to encode the emailadress so harvesters can't get it.
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place an emailadress in your post or page, select it and press the email button in the WYSIWYG editor
+
+== Frequently Asked Questions ==
+
+= How do I use it in the plain HTML editor? =
+
+Put the email adress in the [email] shortcode like this: [email]email@myhost.com[/email]
+
+== Changelog ==
+
+= 1.0 =
+* First release
+
